cb66d0098e512969d0dde5772064229e27cc3ebc
Oleg Golibroda <oleg.golibroda@iway.ch>
2021-08-13 16:22:38 +0200

    security und beter forms

--- room_manager/forms.py
+++ room_manager/forms.py
@@ -7,6 +7,13 @@ class RoomForm(forms.Form):
     person = forms.CharField(max_length=256, required=False)
 
 class HandingOverForm(forms.Form):
+    HandingOverForm = forms.CharField()
     date = forms.DateField(required=False)
     person = forms.CharField(max_length=256, required=False)
     description = forms.CharField(required=False)
+
+class ImageForm(forms.Form):
+    ImageForm = forms.CharField()
+    immage_id = forms.IntegerField(required=False)
+    immage_img = forms.FileField(required=False)
+    immage_description = forms.CharField(required=False)
--- room_manager/views.py
+++ room_manager/views.py
@@ -1,18 +1,27 @@
 from django import forms
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 from . import models
 from . import forms
 
+
+@login_required
 def index(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
     context =  {
         # 'new_room' : None,
         'rooms' : models.Room.objects.all(),
     }
     return render(request, 'index.html', context=context)
 
+
+@login_required
 def room(request, room_id = None):
+    if not request.user.is_authenticated:
+        return redirect('login')
     if room_id  == None:
         room =  models.Room.objects.create()
         return redirect('room', room_id=room.id)
@@ -22,11 +31,8 @@ def room(request, room_id = None):
     if request.method == 'POST':
         form = forms.RoomForm(request.POST, request.FILES)
         if form.is_valid():
-
             room.name = form.cleaned_data['name']
-
             room.description = form.cleaned_data['description']
-
             room.person = form.cleaned_data['person']
             if form.cleaned_data['img']:
                 room.img = form.cleaned_data['img']
@@ -42,13 +48,39 @@ def room(request, room_id = None):
     }
     return render(request, 'room.html', context=context)
 
+@login_required
 def handover(request, room_id,  handover_id=None):
+
     if handover_id  == None:
         room =  models.Room.objects.get(id = room_id)
         handover =  models.HandingOver.objects.create(room=room)
         return redirect('handover',room_id=room_id, handover_id=handover.id)
     else:
         handover =  models.HandingOver.objects.get(id = handover_id)
+    
+    if request.method == 'POST':
+        form = forms.HandingOverForm(request.POST, request.FILES)
+        if form.is_valid():
+            handover.description = form.cleaned_data['description']
+            handover.person = form.cleaned_data['person']
+            handover.date = form.cleaned_data['date']
+            handover.save()
+    
+    if request.method == 'POST':
+        handover =  models.HandingOver.objects.get(id = handover_id)
+        form = forms.ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("valiiiiid slave")
+            if form.cleaned_data['immage_id']:
+                img = models.Image.objects.get(id=form.cleaned_data['immage_id'])
+            else:
+                img = models.Image.objects.create(handing_over=handover)
+
+            img.description = form.cleaned_data['immage_description']
+            if form.cleaned_data['immage_img']:
+                img.img = form.cleaned_data['immage_img']
+
+            img.save()
     
     context =  {
         # 'new_room' : None,
